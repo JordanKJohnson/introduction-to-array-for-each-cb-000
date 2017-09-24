28065264747c2d173c3a9670d800a5c8315ab0e9
Jordan Johnson <jkj86@hotmail.com>
2017-09-24 14:47:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-// Add your doToElementsInArray() function here:
+var evens = [0, 2, 4, 6, 8, 10];
 
+function evenFn(even, index, array) {
+  console.log(`${even} is not odd!`);
+}
+ 
+for (let i = 0; i < evens.length; i++) {
+  evenFn(evens[i], i, evens);
+}
 
-// Add your changeCompletely() function here:
+doToElementsInArray(array, callback){
+  array.forEach(callback);
+}
+
+changeCompletely(element, index, array){
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + '${array[index]}s!!!';
+}
